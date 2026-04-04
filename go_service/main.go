c913439ced35f929317c3b245aeb9b538580a37e
Alina Holhalova <holhalovaa@mail.ru>
2026-04-04 22:42:43 +0300

    fix(M2): move time.Since after next handler in logging middleware

--- go_service/main.go
+++ go_service/main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "time"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 )
 
 // Middleware для логирования
 func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        start := time.Now()
-        log.Printf("[%s] %s %s", r.Method, r.URL.Path, time.Since(start))
-        next(w, r)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next(w, r) // ← сначала вызвать handler
+		log.Printf("[%s] %s %s", r.Method, r.URL.Path, time.Since(start))
+	}
 }
 
 // Обработчик для проверки
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from Go with logging!")
+	fmt.Fprintf(w, "Hello from Go with logging!")
 }
 
 func main() {
-    http.HandleFunc("/", loggingMiddleware(helloHandler))
-    fmt.Println("Go сервер запущен на порту 8081")
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/", loggingMiddleware(helloHandler))
+	fmt.Println("Go сервер запущен на порту 8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
